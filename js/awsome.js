51b636a2bcf95cd098e4e084f83f3f3ab0cc185c
Mozin Omer <mozinomer12@gmail.com>
2019-10-25 14:57:11 +0500

    dont know where was i i was doing something intresting now i was doing muscle factory again

--- js/awsome.js
+++ js/awsome.js
@@ -1,1 +1,14 @@
-awsome.js
+$(document).ready( function(e) {
+	// adding class active to main selector left side
+	$(".main-selector-column ul li a").click(function(e){
+		e.preventDefault();
+		$('.main-selector-column ul li').removeClass("active");
+		$(this).parent().addClass("active")
+	})
+	// adding class active to inner selector left side
+	$(".inner-selector-column ul li a").click(function(e){
+		e.preventDefault();
+		$('.inner-selector-column ul li').removeClass("active");
+		$(this).parent().addClass("active")
+	})
+})
